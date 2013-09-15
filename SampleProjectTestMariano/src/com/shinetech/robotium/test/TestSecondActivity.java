c1b152729523e1062e0da488fe5cb6ced8a24f29
Maciano <mariano.giardina@gmail.com>
2013-09-15 13:21:42 -0700

    test send text added

--- SampleProjectTestMariano/src/com/shinetech/robotium/test/TestSecondActivity.java
+++ SampleProjectTestMariano/src/com/shinetech/robotium/test/TestSecondActivity.java
@@ -1,10 +1,14 @@
 package com.shinetech.robotium.test;
 
 import com.jayway.android.robotium.solo.Solo;
-import com.shinetech.robotium.Main;
 import com.shinetech.robotium.NewActivity;
+import com.shinetech.robotium.R;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class TestSecondActivity extends
 		ActivityInstrumentationTestCase2<NewActivity> {
@@ -25,5 +29,30 @@ public class TestSecondActivity extends
 		solo.assertCurrentActivity("Check on second activity", NewActivity.class);
 	}
 	
+	public void testSendText(){
+		final String sendedText = "texto prueba";
+		
+		final TextView label03=(TextView) solo.getCurrentActivity().findViewById(R.id.example_03_text_view);
+		assertEquals("yay, we got here!",label03.getText().toString());
+		
+		final EditText editfield= (EditText) solo.getCurrentActivity().findViewById(R.id.example_04_edit_text);
+		
+		solo.enterText(editfield, sendedText);
+		
+		final Button button04 = (Button) solo.getCurrentActivity().findViewById(R.id.example_04_button);
+		solo.clickOnView(button04);
+		
+		final TextView label04 = (TextView) solo.getCurrentActivity().findViewById(R.id.example_04_text_view);
+		assertEquals(sendedText,label04.getText().toString());
+		
+		
+		
+	}
+	
+	public void testAnimateEvent(){
+		
+		
+		
+	}
 	
 }
